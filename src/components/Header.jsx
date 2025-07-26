afd241d7d3fa4aa3fdc55676a6ebcab5ad7fc013
No_otra_vez <saulbector@gmail.com>
2025-07-26 14:46:38 -0600

    Wrapper component and style update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,14 @@ import ReactNav from "./Nav";
 export default function ReactHeader() {
     return (
         <header className="
+        min-h-14
         relative
-        pt-4 lg:pt-12 
-        px-4 lg:pl-12 lg:pr-0
+        pt-6 lg:pt-12 
+        px-6 lg:pl-12 lg:pr-0
         flex flex-row lg:flex-col
         ">
             <h1 className="
+            absolute lg:static
             text-2xl
             lg:pb-20 
             font-semibold tracking-widest
@@ -21,7 +23,7 @@ export default function ReactHeader() {
             overflow-auto no-scrollbar">
                 <ReactNav />
             </div>
-            <div className="absolute right-4 items-center">
+            <div className="absolute right-6 items-center">
                 <a href="#">
                     <img className="block size-8 lg:hidden" src="/menu-icon.svg" alt="Menú" />
                 </a>
